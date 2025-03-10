f79439ab497da41799af37cbee223d19507b4b1b
cveditor <cveditorai@gmail.com-NOREPLY>
2025-03-10 20:53:54 +0100

    fix 3.1

--- config/passport.js
+++ config/passport.js
@@ -1,67 +1,29 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const InstagramStrategy = require('passport-instagram').Strategy;
-const TikTokStrategy = require('passport-tiktok').Strategy;
-const User = require('../models/User');
+const User = require('../models/User'); // Modello utente
 require('dotenv').config();
 
-const calculateTrialEndDate = () => {
-  const trialEndDate = new Date();
-  trialEndDate.setDate(trialEndDate.getDate() + 7);
-  return trialEndDate;
-};
-
-const saveUser = async (profile, done) => {
-  try {
-    let user = await User.findOne({ where: { email: profile.emails[0]?.value } });
-
-    if (!user) {
-      user = await User.create({
-        username: profile.displayName,
-        email: profile.emails[0]?.value,
-        provider: profile.provider,
-        plan: 'trial',
-        trialEnd: calculateTrialEndDate(),
-      });
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: `${process.env.BACKEND_URL}/api/auth/google/callback`
+  },
+  async (accessToken, refreshToken, profile, done) => {
+    try {
+      let user = await User.findOne({ where: { googleId: profile.id } });
+      if (!user) {
+        user = await User.create({
+          googleId: profile.id,
+          username: profile.displayName,
+          email: profile.emails[0].value
+        });
+      }
+      return done(null, user);
+    } catch (err) {
+      return done(err);
     }
-    return done(null, user);
-  } catch (err) {
-    return done(err);
   }
-};
-
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: `${process.env.BACKEND_URL}/api/auth/google/callback`,
-    },
-    (accessToken, refreshToken, profile, done) => saveUser(profile, done)
-  )
-);
-
-passport.use(
-  new InstagramStrategy(
-    {
-      clientID: process.env.INSTAGRAM_CLIENT_ID,
-      clientSecret: process.env.INSTAGRAM_CLIENT_SECRET,
-      callbackURL: `${process.env.BACKEND_URL}/api/auth/instagram/callback`,
-    },
-    (accessToken, refreshToken, profile, done) => saveUser(profile, done)
-  )
-);
-
-passport.use(
-  new TikTokStrategy(
-    {
-      clientID: process.env.TIKTOK_CLIENT_ID,
-      clientSecret: process.env.TIKTOK_CLIENT_SECRET,
-      callbackURL: `${process.env.BACKEND_URL}/api/auth/tiktok/callback`,
-    },
-    (accessToken, refreshToken, profile, done) => saveUser(profile, done)
-  )
-);
+));
 
 passport.serializeUser((user, done) => {
   done(null, user.id);
@@ -75,5 +37,6 @@ passport.deserializeUser(async (id, done) => {
     done(err);
   }
 });
+
 
 module.exports = passport;
